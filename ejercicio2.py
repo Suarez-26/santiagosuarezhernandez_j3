59a02c7cefd80d4d211af59ca95c03ae079c0fe3
suarez-26 <217262953+Suarez-26@users.noreply.github.com>
2025-07-11 16:30:54 -0500

    examen

---
+++ ejercicio2.py
@@ -0,0 +1,10 @@
+# pedir al usuario el valor del lado
+lado = float(input("Ingrese la longitud del lado del triangulo equilatero :"))
+lado2 = float(input("Ingrese la longitud del lado del triangulo equilatero  :"))
+lado3 = float(input("Ingrese la longitud del lado del triangulo equilatero :"))
+
+raiz3 = 1.732
+
+area = (raiz3 / 4 ) * lado ** 2
+
+print(f'El area del triangulo equilatero es :',area)
